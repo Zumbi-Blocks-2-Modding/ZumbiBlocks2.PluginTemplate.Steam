deefdd841d44c876c38b2c839f3d787b37a982d0
nyawoi <106277673+nyawoi@users.noreply.github.com>
2024-01-31 14:42:33 -0600

    Convert to dotnet template

--- ExamplePlugin/ExamplePlugin.cs
+++ ExamplePlugin/ExamplePlugin.cs
@@ -6,10 +6,10 @@ namespace ExamplePlugin;
 public class ExamplePlugin : BaseUnityPlugin
 {
     public const string PluginGUID = PluginAuthor + "." + PluginName;
-    public const string PluginAuthor = "PluginAuthor";
-    public const string PluginName = "PluginName";
+    public const string PluginAuthor = "AuthorName";
+    public const string PluginName = "ExamplePlugin";
     public const string PluginVersion = "0.1.0";
-    
+
     private void Awake()
     {
         Logger.LogInfo($"Initialized plugin: {PluginName} ({PluginGUID}@{PluginVersion})");
